4161f5cec25d8449a5771f7f33b53950de983185
sseron <samuelseron@gmail.com>
2015-01-05 09:24:16 +0100

    [REF]Change conditions term modification...

--- src/main/java/org/kerwyn/game/service/UserServiceImpl.java
+++ src/main/java/org/kerwyn/game/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.kerwyn.game.repositories.UserRepository;
 import org.kerwyn.game.service.exception.UserAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 
 @Service
@@ -15,18 +16,23 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private AuthorityRepository authorityRepository;
-	
+
 	@Override
 	public User save(User user) {
-		User existing_user = userRepository.findOneByUsername(user.getUsername());
-		if (existing_user != null || user.getUsername() == "" || user.getPassword() == "") {
-            throw new UserAlreadyExistsException(
-                    String.format("There already exists a user with username=%s", user.getUsername()));
-        }
-		authorityRepository.save(new Authority(user.getUsername(), "ROLE_USER"));
+		User existing_user = userRepository.findOneByUsername(user
+				.getUsername());
+
+		if (existing_user != null || StringUtils.isEmpty(user.getUsername())
+				|| StringUtils.isEmpty(user.getPassword())) {
+			throw new UserAlreadyExistsException(String.format(
+					"There already exists a user with username=%s",
+					user.getUsername()));
+		}
+		authorityRepository
+				.save(new Authority(user.getUsername(), "ROLE_USER"));
 		return userRepository.save(user);
 	}
 
